07dc2e1bbff1bb78cf9eb8972040c535a1bd5aa2
Sydney Quintanilla <sydneyquintanilla@Sydneys-MacBook-Pro.local>
2026-09-22 10:01:00 +1000

    added payroll and overtime calculations to calculate.java

--- src/models/calculate.java
+++ src/models/calculate.java
@@ -1,5 +1,17 @@
 package models;
 
 public class calculate {
+
+    public static int payroll(int hoursWorked, int hourlyRate) {
+        return hoursWorked * hourlyRate;
+    }
+    
+    public static int overtime(int hoursWorked, int hourlyRate) {
+        if (hoursWorked > 40) {
+            int overtimeHours = hoursWorked - 40;
+            return overtimeHours * hourlyRate * 2; // Overtime pay is double the hourly rate
+        }
+        return 0; // No overtime pay if hours worked is 40 or less
+    }
     
 }
