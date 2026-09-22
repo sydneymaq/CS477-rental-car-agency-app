86c4aec9c2c863587ba21d8a674cac2653ebc589
Sydney Quintanilla <sydneyquintanilla@Sydneys-MacBook-Pro.local>
2026-09-22 10:14:03 +1000

    added basic employee classes and admin

--- src/models/admin.java
+++ src/models/admin.java
@@ -1,5 +1,20 @@
 package models;
 
 public class admin {
+    private String name;
+    private int id;
+
+    public admin(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
     
 }
--- src/models/employee.java
+++ src/models/employee.java
@@ -1,5 +1,20 @@
 package models;
 
 public class employee {
+    private String name;
+    private int id;
+
+    public employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
     
 }
